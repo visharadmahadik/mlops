573078cf1cdf891915100aa6acc8ebeb2842db55
Visharad29 <visharad@Visharads-MacBook-Air.local>
2025-01-15 14:59:34 +0530

    model files

---
+++ src/preprocessing.py
@@ -0,0 +1,36 @@
+import os
+import pandas as pd
+
+def get_data(file_path):
+    df_iris = pd.read_csv(file_path, header=None)
+    return df_iris
+
+
+# Main execution block
+if __name__ == "__main__":
+    
+    # Input and output paths from SageMaker Processing
+    input_path = "/opt/ml/processing/input"
+    output_path = "/opt/ml/processing/output"
+    
+    # Ensure output directory exists
+    os.makedirs(output_path, exist_ok=True)
+
+    # Find the input file (assuming there's only one)
+    input_files = os.listdir(input_path)
+    if not input_files:
+        raise ValueError("No input files found in the input directory")
+    
+    # input_file_path = os.path.join(input_path, input_files[0])
+    input_file_path = os.path.join(input_path, 'iris.csv') 
+    output_file_path = os.path.join(output_path, "preprocessed_iris.csv") 
+    
+
+     # Execute loading step
+    print(f"Loading data from: {input_file_path}")
+    processed_data = get_data(input_file_path)
+    
+    print(f"Saving processed data to: {output_file_path}")
+    processed_data.to_csv(output_file_path, index=False, header=False)
+    
+    print("Processing complete!") 
---
+++ src/train.py
@@ -0,0 +1,60 @@
+import mlflow.xgboost
+from sklearn.datasets import load_iris
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import accuracy_score
+import xgboost as xgb
+import pandas as pd
+import os
+import mlflow
+from mlflow import MlflowClient
+
+def train_data():
+    train_data= '/opt/ml/processing/input/train'
+    
+     # Read input files 
+    input_files = [
+        os.path.join(train_data, file) 
+        for file in os.listdir(train_data) 
+        if os.path.isfile(os.path.join(train_data, file))
+    ]
+
+    if not input_files:
+        raise ValueError('No input files found in the training directory')
+    
+    raw_data = [pd.read_csv(file, header=None, engine="python") for file in input_files]
+    data = pd.concat(raw_data)
+
+    print(f"Training data shape: {train_data.shape}")
+
+     # Set MLflow tracking URI (if needed)
+    tracking_uri = os.environ.get('MLFLOW_TRACKING_URI')
+
+
+    if tracking_uri is None:
+        print('uri not found in environment')
+        tracking_uri = 'arn:aws:sagemaker:us-east-1:750573229682:mlflow-tracking-server/mlflow-tracking-server-sagemaker-poc'
+
+    mlflow.set_tracking_uri(tracking_uri)
+
+    X = pd.DataFrame(data.data, columns=data.feature_names)
+    y = data.target
+
+    # Split the data
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+
+    # Step 2: Train XGBoost model
+    model = xgb.XGBClassifier(use_label_encoder=False, eval_metric='logloss')
+    model.fit(X_train, y_train)
+    
+    # Step 3: Log model in MLflow
+    mlflow.set_experiment("xgboost_iris_experiment")
+    with mlflow.start_run() as run:
+        # Log model
+        mlflow.xgboost.log_model(model, artifact_path="xgboost_model")
+        print(f"Model logged in MLflow with run ID: {mlflow.active_run().info.run_id}")
+        mlflow.log_param("test_size",0.2)
+        mlflow.log_param("random_state",42)
+        
+
+if __name__ == "__main__":
+    train_data()
